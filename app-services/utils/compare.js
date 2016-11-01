e698882d44c1b8b9d3bc7fd44c10601005fa9231
ONSINEE CHONGSERMSIRISAKUL <s76668@scbcorp.co.th>
2016-11-01 17:22:35 +0700

    add compare directive

---
+++ app-services/utils/compare.js
@@ -0,0 +1,17 @@
+(function() {
+'use strict';
+angular.module('angularUtils.directives.comparePassword', [])
+    .directive('pwCheck', [function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, elem, attrs, ctrl) {
+            var firstPassword = '#' + attrs.pwCheck;
+            elem.add(firstPassword).on('keyup', function () {
+                scope.$apply(function () {
+                    ctrl.$setValidity('pwmatch', elem.val() === $(firstPassword).val());
+                });
+            });
+        }
+    }
+}]);
+})();
